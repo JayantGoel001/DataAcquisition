9dbee534e0b23bff6ed9e588d37d2eb613f40f07
CormacKrum <54479676+CormacKrum@users.noreply.github.com>
2020-06-13 14:20:35 +0530

    Add files via upload

--- WebCrawlerUsingScrapy/WebCrawlerUsingScrapy/spiders/quotes_spider.py
+++ WebCrawlerUsingScrapy/WebCrawlerUsingScrapy/spiders/quotes_spider.py
@@ -6,7 +6,6 @@ class QuotesSpider(scrapy.Spider):
     def start_requests(self):
         urls = [
             'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -14,6 +13,22 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         page = response.url.split("/")[-2]
         filename = 'quotes-%s.html' % page
+
+        for q in response.css("div.quote"):
+            text = q.css('span.text::text').get()
+            author = q.css('small.author::text').get()
+            tags = q.css('a.tag::text').getall()
+
+            yield{
+                'text':text,
+                'author':author,
+                'tags':tags,
+            }
+        next_page= response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page,callback=self.parse)
+        
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
